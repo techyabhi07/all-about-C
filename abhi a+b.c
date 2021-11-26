1e1ef3b6f6bee96b2f3288ddd401444c00496b10
Abhinav Raj <abhi@Abhinavs-MacBook-Air.local>
2021-11-26 00:44:05 -0800

    wip

---
+++ abhi a+b.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{int a;
+int b;
+
+printf("enter the first number:");
+scanf("%d ,&a");
+
+printf("enter the second number:");
+scanf("%d ,&b)");
+
+int c=a+b;
+printf("sum of two number is : %d");
+
+
+
+}
---
+++ abhiraj.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 4;
+    
+float b =8.5;
+    char c = 'u';
+    int d=45;
+    int e=45+4;
+    printf ("the value of a is %d \n", a);
+    printf ("the value of b is %f \n", c );
+    printf("the value of c is %c \n", c);
+    printf("sum of a and d is %d \n", a-d);
+printf ("the sum of a and d is %d \n", e);
+return 0;
+}
+
