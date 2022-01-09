d424a3d6c349e258ff4a1f37a603c305452cb76c
Abhinav raj <94953095+techyabhi07@users.noreply.github.com>
2022-01-09 23:59:59 +0530

    Add files via upload

---
+++ random7repeat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+double number;
+char alphabet;
+printf("enter input value");
+scanf("%lf" , & number);
+scanf("%c" ,&alphabet);
+
+printf("number: %lf", number);
+printf("\ncharacter: %d" ,alphabet);
+
+return 0;
+
+}
